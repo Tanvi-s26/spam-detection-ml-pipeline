46b91a2780ba164939bb877ed54e5c9aed57f180
Tanvi <tanviijanday@gmail.com>
2026-04-25 12:20:22 +0530

    Day 2: Added Flask API for spam prediction

---
+++ app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+# Load trained model
+with open("model.pkl", "rb") as f:
+    model, vectorizer = pickle.load(f)
+
+@app.route("/")
+def home():
+    return "Spam Detection API is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    # Get text input
+    text = data.get("text")
+
+    if not data or "text" not in data:
+        return jsonify({"error": "Invalid input"}), 400
+
+    # Convert to model input
+    X = vectorizer.transform([text])
+
+    # Predict
+    prediction = model.predict(X)[0]
+
+    result = "Spam" if prediction == 1 else "Not Spam"
+
+    return jsonify({
+        "input": text,
+        "prediction": result
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
